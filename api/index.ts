25dab49fe4b947052a5ec9eae85e716315fe321c
rcarraroia <rcarrarocoach@gmail.com>
2025-11-11 19:19:30 -0300

    fix: Corrigir configuração do Vercel para deploy correto do frontend

--- api/index.ts
+++ api/index.ts
@@ -3,6 +3,9 @@
  * Wrapper para o Express app
  */
 
+import type { VercelRequest, VercelResponse } from '@vercel/node';
 import app from '../src/server';
 
-export default app;
+export default async (req: VercelRequest, res: VercelResponse) => {
+  return app(req, res);
+};
